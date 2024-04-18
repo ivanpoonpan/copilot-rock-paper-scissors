70ded0d8a099f74778b7bd01a570bd11d45a247d
ivanpoonpan <longlong615615615@gmail.com>
2024-04-18 15:53:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
-# Write a rock, paper, scissors game
-# import random module
+from flask import Flask, request, jsonify
 import random
-# define main function that handles all the logic
-def main():
-    # define a list of valid choices
-    choices = ['rock', 'paper', 'scissors']
-    # define a variable to store the user choice
-    user_choice = ''
-    # define a variable to store the computer choice
-    computer_choice = ''
-    # define a variable to store the result
-    result = ''
-    # loop until the user has made a valid choice
-    while user_choice not in choices:
-        # get the user choice
-        user_choice = input('Enter rock, paper, or scissors: ')
-    # get the computer choice
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def play_game():
+    user_choice = request.json['choice']
+    choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+    if user_choice not in choices:
+        return jsonify({'error': 'Invalid choice'}), 400
+
     computer_choice = random.choice(choices)
-    # determine the result
     if user_choice == computer_choice:
         result = 'It\'s a tie!'
-    elif user_choice == 'rock' and computer_choice == 'scissors':
-        result = 'You win!'
-    elif user_choice == 'paper' and computer_choice == 'rock':
-        result = 'You win!'
-    elif user_choice == 'scissors' and computer_choice == 'paper':
+    elif (user_choice == 'rock' and (computer_choice == 'scissors' or computer_choice == 'lizard')) or \
+         (user_choice == 'paper' and (computer_choice == 'rock' or computer_choice == 'spock')) or \
+         (user_choice == 'scissors' and (computer_choice == 'paper' or computer_choice == 'lizard')) or \
+         (user_choice == 'lizard' and (computer_choice == 'spock' or computer_choice == 'paper')) or \
+         (user_choice == 'spock' and (computer_choice == 'scissors' or computer_choice == 'rock')):
         result = 'You win!'
     else:
         result = 'You lose!'
-    # print the result
-    print(f'You chose {user_choice}, the computer chose {computer_choice}. {result}')
+    return jsonify({'result': result}), 200
 
-    # call main function
-    main()
+if __name__ == "__main__":
+    app.run(debug=True)
